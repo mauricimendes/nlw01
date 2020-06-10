10f42223f3211d09c3a10a33ede5bca8976d6799
mauricimendes <mauricimendes.14@gmail.com>
2020-06-10 19:09:55 -0300

    imagens

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemController {
             return { 
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.1.108:3333/uploads/${item.image}`
+                image_url: `http://seu_ip_externo:3333/uploads/${item.image}`
             }
         })
         
